5067124db5ef39352ac787894ede12fa53186e33
LiquidCGS <marcos2arroyo@yahoo.com.ar>
2021-04-22 18:54:32 -0300

    Updated controller firmware to work with new data structure

--- Software/Firmware/Controller_INDEX/Left_controller/Left_controller.ino
+++ Software/Firmware/Controller_INDEX/Left_controller/Left_controller.ino
@@ -23,54 +23,41 @@
 #define IB_BTouch           0x0008
 #define IB_SYSClick         0x0010
 #define IB_ThumbStickClick  0x0020
-#define IB_FingerIndex      0x0040
-#define IB_FingerMiddle     0x0080
-#define IB_FingerRing       0x0100
-#define IB_FingerPinky      0x0200
-#define IB_TrackpadTouch    0x0400
-#define IB_ThumbStickTouch  0x0800
+#define IB_TrackpadTouch    0x0040
+#define IB_ThumbStickTouch  0x0080
 
 #define BatLevelMax         968             //you need to find all of these values on your own
 #define JoyXMin             237             //check on the utils folder for sketches and instructions
 #define JoyXMax             935             //that help on getting these values
 #define JoyYMin             190             //YOU NEED TO DO THIS FOR BOTH CONTROLLERS
 #define JoyYMax             900             //if you use these values without changing them you MAY
-#define JoyXDeadZoneMin     525             //get stick drift
-#define JoyXDeadZoneMax     580
-#define JoyYDeadZoneMin     460
-#define JoyYDeadZoneMax     530
-
-#define CTRLQW        0
-#define CTRLQX        1
-#define CTRLQY        2
-#define CTRLQZ        3
-#define CTRLBTN       4
-#define CTRLTRIGG     5
-#define CTRLAXISX     6
-#define CTRLAXISY     7
-#define CTRLTRACKY    8
-#define CTRLVBAT      9
+#define JoyXDeadZoneMin     515             //get stick drift
+#define JoyXDeadZoneMax     590
+#define JoyYDeadZoneMin     440
+#define JoyYDeadZoneMax     600
 
 
 //uint64_t Pipe = 0xF0F0F0F0E1LL; //right
 uint64_t Pipe = 0xF0F0F0F0D2LL; //left
 
-struct ctrlData{
+struct ctrlData {
   float qW;
   float qX;
   float qY;
   float qZ;
-  float BTN;
-  short  trigg;
-  short  axisX;
-  short  axisY;
-  short  trackY;
-  short  vBAT;
+  uint32_t BTN;
+  uint8_t  trigg;
+  int8_t  axisX;
+  int8_t  axisY;
+  int8_t  trackY;
+  uint8_t  vBAT;
+  uint8_t  fingerThumb;
+  uint8_t  fingerIndex;
+  uint8_t  fingerMiddle;
+  uint8_t  fingerRing;
+  uint8_t  fingerPinky;
 };
 
-float trigger;
-float axisXout;
-float axisYout;
 int tracky;
 int trackoutput;
 int axisX;
@@ -80,8 +67,6 @@ RF24 radio(9, 10);
 
 
 void setup() {
-  Serial.begin(9600);
-  
   pinMode(APin, INPUT_PULLUP);
   pinMode(BPin, INPUT_PULLUP);
   pinMode(SysPin, INPUT_PULLUP);
@@ -97,12 +82,11 @@ void setup() {
   radio.openWritingPipe(Pipe);
   radio.startListening();
   radio.setAutoAck(false);
-  
+
 }
 
 void loop() {
   mympu_update();
-
   ctrlData data;
   int btn = 0;
   tracky = analogRead(TrackpadPin);
@@ -126,24 +110,25 @@ void loop() {
   axisY = analogRead(JoyYPin);
 
   if (axisX > JoyXDeadZoneMax || axisX < JoyXDeadZoneMin) {
-    axisXout = -mapFloat(axisX, JoyXMin, JoyXMax, -1, 1);
+    data.axisX = mapFloat(axisX, JoyXMin, JoyXMax, -127, 127);
   } else {
-    axisXout = 0;
+    data.axisX = 0;
   }
 
   if (axisY > JoyYDeadZoneMax || axisY < JoyYDeadZoneMin) {
-    axisYout = mapFloat(axisY, JoyYMin, JoyYMax, -1, 1);
+    data.axisY = -mapFloat(axisY, JoyYMin, JoyYMax, -127, 127);
     btn |= IB_ThumbStickTouch;
   } else {
-    axisYout = 0;
+    data.axisY = 0;
   }
 
 
   if (!digitalRead(TriggerPin)) {
-    trigger = 1;
-    btn |= IB_FingerIndex;
+    data.trigg = 255;
+    data.fingerIndex = 255;
   } else {
-    trigger = 0;
+    data.trigg = 0;
+    data.fingerIndex = 0;
   }
 
   if (!digitalRead(APin)) {
@@ -160,17 +145,29 @@ void loop() {
   if (!digitalRead(JoyClickPin)) {
     btn |= IB_ThumbStickClick;
   }
-  if (digitalRead(FingerIndexPin)) {
-    btn |= IB_FingerIndex;
-  }
+//  if (digitalRead(FingerIndexPin)) {
+//    data.fingerIndex = 255;
+//  }
+//  else {
+//    data.fingerIndex = 0;
+//  }
   if (digitalRead(FingerMiddlePin)) {
-    btn |= IB_FingerMiddle;
+    data.fingerMiddle = 255;
+  }
+  else {
+    data.fingerMiddle = 0;
   }
   if (digitalRead(FingerRingPin)) {
-    btn |= IB_FingerRing;
+    data.fingerRing = 255;
+  }
+  else {
+    data.fingerRing = 0;
   }
   if (digitalRead(FingerPinkyPin)) {
-    btn |= IB_FingerPinky;
+    data.fingerPinky = 255;
+  }
+  else {
+    data.fingerPinky = 0;
   }
 
   data.qW = mympu.qW;
@@ -178,11 +175,8 @@ void loop() {
   data.qZ = mympu.qZ;
   data.qX = mympu.qX;
   data.BTN = btn;
-  data.trigg = (trigger * 100);
-  data.axisX = (-axisXout * 100);
-  data.axisY = (-axisYout * 100);
-  data.trackY = (trackoutput * 100);
-  data.vBAT = (mapFloat(analogRead(VbatPin), 787, BatLevelMax, 0, 1) * 100);
+  data.trackY = (trackoutput * 127);
+  data.vBAT = (mapFloat(analogRead(VbatPin), 787, BatLevelMax, 0, 255));
   radio.stopListening();
   radio.write(&data, sizeof(ctrlData));
   radio.startListening();
--- Software/Firmware/Controller_INDEX/Right_controller/mpu.h
+++ Software/Firmware/Controller_INDEX/Right_controller/mpu.h
@@ -2,8 +2,9 @@
 #define MPU_H
 
 struct s_mympu {
-	float ypr[3];
-	float gyro[3];
+  float gyro[3];
+  float accel[3];
+  float comp[3];
   float qW, qX, qY, qZ;
 };
 
@@ -11,5 +12,6 @@ extern struct s_mympu mympu;
 
 int mympu_open(unsigned int rate);
 int mympu_update();
+int mympu_update_compass();
 
 #endif
