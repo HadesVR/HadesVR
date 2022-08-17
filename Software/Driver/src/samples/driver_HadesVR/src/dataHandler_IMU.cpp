66255bce22c1b1d5796f1d3071d18273f315bb8a
LiquidCGS <marcos2arroyo@yahoo.com.ar>
2022-08-17 01:45:09 -0300

    Cleanup

--- Software/Driver/src/samples/driver_HadesVR/src/dataHandler_IMU.cpp
+++ Software/Driver/src/samples/driver_HadesVR/src/dataHandler_IMU.cpp
@@ -1,11 +1,14 @@
 #include "dataHandler.h"
 
-void CdataHandler::CalcIMUVelocity(_ControllerData& _data) 
+void CdataHandler::CalcIMUPosition(_TrackingData& _data, V3Kalman& k)
 {
+	//don't update on old data
+	if (_data.Accel == _data.oldAccel) return;
+
 	//get deltatime
 	auto now = std::chrono::high_resolution_clock::now();
-	deltatime = std::chrono::duration_cast<std::chrono::microseconds>(now - _data.lastUpdate).count() / 1000000.0f;
-	_data.lastUpdate = now;
+	deltatime = std::chrono::duration_cast<std::chrono::microseconds>(now - _data.lastIMUUpdate).count() / 1000000.0f;
+	_data.lastIMUUpdate = now;
 
 	//Rotate gravity vector https://web.archive.org/web/20121004000626/http://www.varesano.net/blog/fabio/simple-gravity-compensation-9-dom-imus
 	Vector3 g = Vector3(-(2.0f * (_data.Rotation.X * _data.Rotation.W - _data.Rotation.Y * _data.Rotation.Z)),
@@ -18,81 +21,29 @@ void CdataHandler::CalcIMUVelocity(_ControllerData& _data)
 	//convert to m/s^2
 	lin_Acc *= 9.80665f;
 
-	//integrate to get velocity													
-	_data.Velocity += (lin_Acc * deltatime);
+	//integrate to get velocity	(also swap axis)												
+	_data.Velocity += (Vector3(lin_Acc.Y, lin_Acc.X, lin_Acc.Z) * deltatime);
 
-	//decay
-	_data.Velocity *= 0.99f;
+	//again to get position
+	Vector3 instant_pos = (_data.Velocity * deltatime);
+
+	//update filter
+	k.updateMeasIMU(instant_pos);
+
+	//update old accel
+	_data.oldAccel = _data.Accel;
 }
 
-void CdataHandler::CalcIMUVelocity(_HMDData& _data) 
-{
+void CdataHandler::CalcVelocity(_TrackingData& _data) {
+
 	//get deltatime
 	auto now = std::chrono::high_resolution_clock::now();
-	deltatime = std::chrono::duration_cast<std::chrono::microseconds>(now - _data.lastUpdate).count() / 1000000.0f;
-	_data.lastUpdate = now;
+	deltatime = std::chrono::duration_cast<std::chrono::microseconds>(now - _data.lastCamUpdate).count() / 1000000.0f;
+	_data.lastCamUpdate = now;
 
-	//Rotate gravity vector https://web.archive.org/web/20121004000626/http://www.varesano.net/blog/fabio/simple-gravity-compensation-9-dom-imus
-	Vector3 g = Vector3(-(2.0f * (_data.Rotation.X * _data.Rotation.W - _data.Rotation.Y * _data.Rotation.Z)),
-						(2.0f * (_data.Rotation.Y * _data.Rotation.X + _data.Rotation.Z * _data.Rotation.W)),
-						(_data.Rotation.Y * _data.Rotation.Y - _data.Rotation.X * _data.Rotation.X - _data.Rotation.Z * _data.Rotation.Z + _data.Rotation.W * _data.Rotation.W));
+	//update velocity
+	_data.Velocity = (_data.Position - _data.oldPosition) / deltatime;
 
-	//remove gravity vector (or at least attempt to)
-	Vector3 lin_Acc = _data.Accel - g;
-
-	//convert to m/s^2
-	lin_Acc *= 9.80665f;
-
-	//integrate to get velocity													
-	_data.Velocity += (lin_Acc * deltatime);
-
-	//decay
-	_data.Velocity *= 0.99f;
+	//update position
+	_data.oldPosition = _data.Position;
 }
-
-/*
-void CdataHandler::CalcAccelPosition(float quatW, float quatX, float quatY, float quatZ, float accelX, float accelY, float accelZ, PosData& pos) {
-
-	//get time delta
-	auto now = std::chrono::high_resolution_clock::now();
-	deltatime = std::chrono::duration_cast<std::chrono::microseconds>(now - pos.lastUpdate).count() / 1000000.0f;
-	pos.lastUpdate = now;
-
-	//Rotate gravity vector https://web.archive.org/web/20121004000626/http://www.varesano.net/blog/fabio/simple-gravity-compensation-9-dom-imus
-	float gx = (2.0f * (quatX * quatZ - quatW * quatY));
-	float gy = (2.0f * (quatW * quatX + quatY * quatZ));
-	float gz = (quatW * quatW - quatX * quatX - quatY * quatY + quatZ * quatZ);
-
-	float lin_ax = accelX - gx;
-	float lin_ay = accelY - gy;
-	float lin_az = accelZ - gz;
-
-	//convert to m/s^2
-	lin_ax *= 9.80665f;
-	lin_ay *= 9.80665f;
-	lin_az *= 9.80665f;
-
-	//integrate to get velocity														TODO: REWORK ALL THIS CRAP
-	pos.vx += (lin_ay * deltatime);
-	pos.vy += (lin_ax * deltatime);
-	pos.vz += (lin_az * deltatime);
-
-	//integrate to get position
-	pos.posX = ((pos.vx + pos.oldvX) * 0.5f) * deltatime + pos.oldPosX;
-	pos.posY = ((pos.vy + pos.oldvY) * 0.5f) * deltatime + pos.oldPosY;
-	pos.posZ = ((pos.vz + pos.oldvZ) * 0.5f) * deltatime + pos.oldPosZ;
-
-	//decay
-	pos.vx *= 0.8f;
-	pos.vy *= 0.8f;
-	pos.vz *= 0.8f;
-
-	pos.oldvX = pos.vx;
-	pos.oldvY = pos.vy;
-	pos.oldvZ = pos.vz;
-
-	pos.oldPosX = pos.posX;
-	pos.oldPosY = pos.posY;
-	pos.oldPosZ = pos.posZ;
-}
-*/
