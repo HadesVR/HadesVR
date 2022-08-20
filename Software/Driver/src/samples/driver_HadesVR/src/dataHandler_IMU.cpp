69b6a993bb9a1b01aa44434b0c03a77861903abc
LiquidCGS <marcos2arroyo@yahoo.com.ar>
2022-08-20 13:23:35 -0300

    Cleanup

--- Software/Driver/src/samples/driver_HadesVR/src/dataHandler_IMU.cpp
+++ Software/Driver/src/samples/driver_HadesVR/src/dataHandler_IMU.cpp
@@ -1,6 +1,6 @@
 #include "dataHandler.h"
 
-void CdataHandler::UpdateIMUPosition(_TrackingData& _data, V3Kalman& k)
+void CdataHandler::UpdateIMUPosition(_TrackingData& _data, V3Kalman& _k)
 {
 	//don't update on old data
 	if (_data.Accel == _data.oldAccel) return;
@@ -10,12 +10,10 @@ void CdataHandler::UpdateIMUPosition(_TrackingData& _data, V3Kalman& k)
 	deltatime = std::chrono::duration_cast<std::chrono::microseconds>(now - _data.lastIMUUpdate).count() / 1000000.0f;
 	_data.lastIMUUpdate = now;
 
-	Quaternion _q = Quaternion(_data.Rotation.Y, _data.Rotation.Z, _data.Rotation.X, _data.Rotation.W);
-
 	//Rotate gravity vector https://web.archive.org/web/20121004000626/http://www.varesano.net/blog/fabio/simple-gravity-compensation-9-dom-imus
-	Vector3 g = Vector3((2.0f * (_q.X * _q.Y - _q.Z * _q.W)),
-						(2.0f * (_q.X * _q.Z + _q.Y * _q.W)),
-						(_q.X * _q.X - _q.Z * _q.Z - _q.Y * _q.Y + _q.W * _q.W));
+	Vector3 g = Vector3((2.0f * (_data.Rotation.Y * _data.Rotation.Z - _data.Rotation.X * _data.Rotation.W)),
+						(2.0f * (_data.Rotation.Y * _data.Rotation.X + _data.Rotation.Z * _data.Rotation.W)),
+						(_data.Rotation.Y * _data.Rotation.Y - _data.Rotation.X * _data.Rotation.X - _data.Rotation.Z * _data.Rotation.Z + _data.Rotation.W * _data.Rotation.W));
 
 	//remove gravity vector (or at least attempt to)
 	Vector3 lin_Acc = _data.Accel - g;
@@ -27,8 +25,8 @@ void CdataHandler::UpdateIMUPosition(_TrackingData& _data, V3Kalman& k)
 	lin_Acc = Vector3(lin_Acc.Y, lin_Acc.X, lin_Acc.Z);
 
 	//rotate vector to quaternion so it all matches up nicely
-	Quaternion _pq = Quaternion::Inverse(Quaternion(_data.Rotation.X, _data.Rotation.Z, _data.Rotation.Y, _data.Rotation.W));
-	lin_Acc = (_pq * lin_Acc);
+	Quaternion pq = Quaternion::Inverse(Quaternion(_data.Rotation.X, _data.Rotation.Z, _data.Rotation.Y, _data.Rotation.W));
+	lin_Acc = (pq * lin_Acc);
 
 	//integrate to get velocity											
 	_data.Velocity += (lin_Acc * deltatime);
@@ -37,7 +35,7 @@ void CdataHandler::UpdateIMUPosition(_TrackingData& _data, V3Kalman& k)
 	Vector3 instant_pos = (_data.Velocity * deltatime);
 
 	//update filter
-	k.updateMeasIMU(instant_pos);
+	_k.updateMeasIMU(instant_pos);
 
 	//update old accel
 	_data.oldAccel = _data.Accel;
