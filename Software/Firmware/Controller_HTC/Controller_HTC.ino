151fca897572b503c986fbefab6b36cdf993148b
Pierre Ståhl <pierre.sigma@renzgroup.com>
2023-03-14 10:43:03 +0100

    Harmonize HTC controller configuration

--- Software/Firmware/Controller_HTC/Controller_HTC.ino
+++ Software/Firmware/Controller_HTC/Controller_HTC.ino
@@ -23,29 +23,26 @@
 //************************************ USER CONFIGURABLE STUFF HERE*****************************************
 //==========================================================================================================
 
-//#define SERIAL_DEBUG
-#define IMU_ADDRESS     0x68                // You can find it out by using the IMUIdentifier example
-MPU9250 IMU;                                // IMU type
-#define CALPIN              4               //pin to start mag calibration at power on
+// Configure controller by either:
+// * Modifying Configuration.h directly OR (preferably):
+// * Making a copy of it, calling it User_Configuration.h and modifying that
+// * Comment or uncomment below for right or left controller
 
-#define SysPin              4
-#define MenuPin             3
-#define GripPin             6
-#define JoyXPin             A1
-#define JoyYPin             A2
-#define JoyClickPin         2
-#define TriggerPin          A3
-#define VbatPin             A0
+// Uncomment/remove for left controller
+#define RIGHT_CONTROLLER
 
-#define BatLevelMax         968             //you need to find all of these values on your own
-#define JoyXMin             200             //check on the utils folder for sketches and instructions
-#define JoyXMax             900             //that help on getting these values
-#define JoyYMin             150             //YOU NEED TO DO THIS FOR BOTH CONTROLLERS
-#define JoyYMax             870             //if you use these values without changing them you MAY
-#define JoyXDeadZoneMin     515             //get stick drift
-#define JoyXDeadZoneMax     590
-#define JoyYDeadZoneMin     430
-#define JoyYDeadZoneMax     560
+//==========================================================================================================
+//************************************ INCLUDE USER CONFIGURATION*******************************************
+//==========================================================================================================
+
+#if __has_include("User_Configuration.h")
+#include "User_Configuration.h"
+#else
+#include "Configuration.h"
+#endif
+
+IMU_TYPE IMU;
+
 //==========================================================================================================
 
 
@@ -100,8 +97,12 @@ bool joyTouch = false;
 //**************************************** RF Data stuff ***************************************************
 //==========================================================================================================
 RF24 radio(9, 10);
-//uint64_t Pipe = 0xF0F0F0F0E1LL; //right
+
+#ifdef RIGHT_CONTROLLER
+uint64_t Pipe = 0xF0F0F0F0E1LL; //right
+#else
 uint64_t Pipe = 0xF0F0F0F0D2LL; //left
+#endif
 //==========================================================================================================
 //**************************************** IMU variables ***************************************************
 //==========================================================================================================
@@ -209,7 +210,7 @@ void setup() {
   data.Data = 0xFF;
 
   filter.begin(MadgwickBeta);
-
+  
   err = IMU.init(calib, IMU_ADDRESS);
   if (err != 0)
   {
